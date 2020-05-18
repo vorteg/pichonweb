f913517d7a80929c8b0be37bf87ef192f7d5cdb6
Qupi <vorteg.r@gmail.com>
2020-05-18 15:42:48 -0500

    Adding Docker-compose

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -4,7 +4,10 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-app = Flask(__name__)
+
+server = Flask(__name__)
+
+
 """
 en este ejercicio se muestra  como renderizar un template de html,por default flask 
 busca los html dentro de una carpeta que se llame templates, si es necesario cambiar ubicacion
@@ -12,10 +15,7 @@ hay que mandar el nombre de la carpeta desde que se instancia, es decir:
 app = Flask(__name__, template_folder = "template_name_folder")
 """
 
-@app.route('/', methods = ['GET','POST'])
+@server.route('/', methods = ['GET','POST'])
 def index(name='Pichones Corp Home'):
     return  render_template('home.html',title = name)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
